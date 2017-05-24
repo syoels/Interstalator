8fb95ca3b36ef3543f8c9eef9b360f7f4b5bcde3
nice-shot <shefferu@gmail.com>
2017-05-24 18:18:07 +0300

    Changed document formatting to Upopa solution

--- Assets/Scripts/components/Component.cs
+++ Assets/Scripts/components/Component.cs
@@ -4,39 +4,40 @@ using UnityEngine;
 
 
 namespace Interstalator {
-	public abstract class Component : MonoBehaviour {
+public abstract class Component : MonoBehaviour {
 
-		public class Connection {
-			public Component connectedComponent; 
-			public ElementTypes type;
-		}
-		public struct TransferredElement {
-			ElementTypes type; 
-			float quantitiy; 
+    public class Connection {
+        public Component connectedComponent;
+        public ElementTypes type;
+    }
 
-			public TransferredElement (ElementTypes t, float amount){
-				type = t; 
-				quantitiy = amount;
-			}
-		}
+    public struct TransferredElement {
+        ElementTypes type;
+        float quantitiy;
+
+        public TransferredElement(ElementTypes t, float amount) {
+            type = t; 
+            quantitiy = amount;
+        }
+    }
 
 
-		public List<Connection> incoming; 
-		public List<Connection> outgoing; 
+    public List<Connection> incoming;
+    public List<Connection> outgoing;
 
-		// Use this for initialization
-		void Start () {
+    // Use this for initialization
+    void Start() {
 			
-		}
+    }
 		
-		// Update is called once per frame
-		void Update () {
+    // Update is called once per frame
+    void Update() {
 			
-		}
+    }
 
-		public abstract void Process (params TransferredElement[] inputs ); 
+    public abstract void Process(params TransferredElement[] inputs);
 
 
 
-	}
+}
 }
