b533718dbf485a62d3ee79912e9041d0b35e94fe
nice-shot <shefferu@gmail.com>
2017-05-27 11:26:19 +0300

    added script - textual component controller

---
+++ Assets/Scripts/TextualComponentController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Interstalator {
+public class TextualComponentController : MonoBehaviour {
+    public string name;
+
+    private Color baseColor;
+
+    private TextMesh statusText;
+    private TextMesh nameText;
+    private SpriteRenderer graphic;
+
+    void Awake() {
+        graphic = transform.FindChild("Graphic").GetComponent<SpriteRenderer>();
+        nameText = transform.FindChild("Name").GetComponent<TextMesh>();
+        statusText = transform.FindChild("Status").GetComponent<TextMesh>();
+
+        nameText.text = name;
+        baseColor = graphic.color;
+    }
+
+    public void SetStatus(string status) {
+        statusText.text = status;
+    }
+
+    void OnMouseEnter() {
+        graphic.color = Color.yellow;
+    }
+
+    void OnMouseExit() {
+        graphic.color = baseColor;
+    }
+
+}
+}
