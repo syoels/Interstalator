3e8b0fa288d40466fa62172dba5674a29958489b
nice-shot <shefferu@gmail.com>
2017-06-01 18:34:20 +0300

    made status text divide into lines to be more readable

--- Assets/Scripts/TextualComponentController.cs
+++ Assets/Scripts/TextualComponentController.cs
@@ -5,6 +5,8 @@ using UnityEngine.EventSystems;
 
 namespace Interstalator {
 public class TextualComponentController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+    private const int STATUS_LINE_SIZE = 17;
+
     private Color baseColor;
 
     private TextMesh statusText;
@@ -20,7 +22,22 @@ public class TextualComponentController : MonoBehaviour, IPointerEnterHandler, I
     }
 
     public void SetStatus(string status) {
-        Debug.Log("Got new status: " + status);
+        if (status.Length > STATUS_LINE_SIZE) {
+            string[] words = status.Split(' ');
+            List<string> newStatus = new List<string>();
+            newStatus.Add("");
+            foreach (string word in words) {
+                int lineIndex = newStatus.Count - 1;
+                if (newStatus[lineIndex] == "") {
+                    newStatus[lineIndex] = word;
+                } else if ((newStatus[lineIndex] + word).Length > STATUS_LINE_SIZE) {
+                    newStatus.Add(word);
+                } else {
+                    newStatus[lineIndex] += " " + word;
+                }
+            }
+            status = string.Join("\n", newStatus.ToArray());
+        }
         statusText.text = status;
     }
 
