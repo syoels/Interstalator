8066d61bcb56f5655a015bd446c8420cb94f7c5c
syoels <syoels@gmail.com>
2017-06-07 14:05:56 +0300

    Added conveyor belt & engine scripts

---
+++ Assets/Scripts/components/ConveyorBelt.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Interstalator {
+public class ConveyorBelt : GenericPipe {
+
+    protected override string ComponentName {
+        get {
+            return "Conveyor Belt";
+        }
+    }
+
+    protected override ElementTypes[] PipeType {
+        get {
+            return new ElementTypes[] {ElementTypes.NuclearWasteRatio};
+        }
+    }
+}
+}
---
+++ Assets/Scripts/components/Engine.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Interstalator {
+public class Engine : ShipComponent {
+
+    public float minElectricity = 0.1f;
+
+    protected override string ComponentName {
+        get {
+            return "Engine";
+        }
+    }
+
+    protected override void SetRequiredInputs() {
+        AddRequiredInput(ElementTypes.Electricity);
+    }
+
+    protected override List<Output> InnerProcess() {
+        List<Output> transmissions = new List<Output>();
+        if (incoming[0].amount < minElectricity) {
+            GraphManager.instance.statusController.SetProblem(
+                ShipStatusController.ShipSystem.Engine,
+                "Engine stopped"
+            );
+        } else {
+            GraphManager.instance.statusController.SetOk(
+                ShipStatusController.ShipSystem.Engine            );
+        }
+        ShipComponent[] conveyerBelts = GetChildrenOfType<ConveyorBelt>();
+        float ratioPerBelt = incoming[0].amount / conveyerBelts.Length;
+        foreach (ConveyorBelt belt in conveyerBelts) {
+            transmissions.Add(new Output(belt, ElementTypes.NuclearWasteRatio, ratioPerBelt));
+        }
+        return transmissions;
+    }
+
+
+}
+}
