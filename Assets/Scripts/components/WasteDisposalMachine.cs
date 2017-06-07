32b2eac9d380ce043eb50ba6d88ad3a45f64c74e
nice-shot <shefferu@gmail.com>
2017-06-07 23:36:10 +0300

    added script for waste disposal machine

---
+++ Assets/Scripts/components/WasteDisposalMachine.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Interstalator {
+public class WasteDisposalMachine : ShipComponent {
+    protected override string ComponentName {
+        get {
+            return "Waste Disposal";
+        }
+    }
+
+    protected override void SetRequiredInputs() {
+        AddRequiredInput(ElementTypes.Electricity);
+        AddRequiredInput(ElementTypes.NuclearWasteRatio);
+    }
+
+    protected override List<Output> InnerProcess() {
+        List<Output> outputs = new List<Output>();
+
+        float incomingElectricity = incoming[0].amount;
+        float incomingWasteRatio = incoming[1].amount;
+        // Outgoing waste is based on electricity but avoids goind below zero
+        float outgoingWasteRatio = Mathf.Max(
+                                  incomingWasteRatio - incomingElectricity,
+                                  0f
+                              );
+
+        float wastePerChild = outgoingWasteRatio / children.Length;
+
+        foreach (ShipComponent child in children) {
+            outputs.Add(new Output(
+                child,
+                ElementTypes.NuclearWasteRatio,
+                wastePerChild
+            ));
+        }
+
+        SetStatus("Failing to dispose of " + outgoingWasteRatio + " waste");
+        return outputs;
+    }
+
+
+}
+}
