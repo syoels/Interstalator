9b812ce7c1715158ec666461e36917dc834d99a3
nice-shot <shefferu@gmail.com>
2017-06-14 15:25:04 +0300

    Added alternative ship component and flow manager scripts that could create simultanious flows

---
+++ Assets/Scripts/alt_components/AltFlowManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Interstalator {
+public class AltFlowManager : FlowManager {
+    private List<AltShipComponent> originList = new List<AltShipComponent>();
+
+    /// <summary>
+    /// The main logic of the flow. Runs as a coroutine to allow adding delays
+    /// to play animations
+    /// </summary>
+    private IEnumerator FlowRoutine() {
+        // Avoids running two flows at once
+        yield return new WaitUntil(() => !runningFlow);
+
+        runningFlow = true;
+
+        originList = new List<AltShipComponent>();
+
+        foreach (AltShipComponent altComp in allComponents) {
+            altComp.ResetIncoming();
+            if (altComp.IsOrigin()) {
+                originList.Add(altComp);
+                StartCoroutine(altComp.StartProcessing(0));
+            }
+        }
+
+        yield return new WaitUntil(() => originProcessing() == false);
+
+        runningFlow = false;
+    }
+
+    private bool originProcessing() {
+        foreach (AltShipComponent ship in originList) {
+            if (ship.isProcessing) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+}
---
+++ Assets/Scripts/alt_components/AltShipComponent.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+// Used to check incoming input
+using System.Linq;
+
+namespace Interstalator {
+public abstract class AltShipComponent : ShipComponent {
+    private bool _isProcessing = false;
+    public bool isProcessing {
+        get {
+            foreach (AltShipComponent child in children) {
+                if (_isProcessing) {
+                    return true;
+                }
+            }
+            return _isProcessing;
+        }
+    }
+
+    public IEnumerator StartProcessing(float waitTime) {
+        Debug.Assert(GetRemainingIncoming() == 0);
+        _isProcessing = true;
+
+        List<Output> outputs = Process();
+
+        // TODO: Handle setting/changing and waiting for state change animation
+        yield return new WaitForSeconds(waitTime);
+
+        _isProcessing = false;
+
+        foreach (Output output in outputs) {
+            AltShipComponent child = (AltShipComponent)output.component;
+            // Deciding on how long to wait should be handled inside StartProcessing
+            float animationTime = child.UpdateInput(output.type, output.amount);
+            if (child.GetRemainingIncoming() == 0) {
+                StartCoroutine(child.StartProcessing(animationTime));
+            }
+        }
+    }
+
+}
+}
