c8023a5e296d57183985f947122e67d202d372dd
Nadav Sheffer <shefferu@gmail.com>
2017-06-24 02:19:01 +0300

    Added delay method for when the state was changed

--- Assets/Scripts/new_components/NewTextualShipComponent.cs
+++ Assets/Scripts/new_components/NewTextualShipComponent.cs
@@ -6,6 +6,7 @@ using UnityEngine.EventSystems;
 namespace Interstalator {
 public abstract class NewTextualShipComponent : NewShipComponent {
     private const int STATUS_LINE_SIZE = 17;
+    private const float DEFAULT_DELAY = 1f;
 
     private Color baseColor;
 
@@ -37,6 +38,13 @@ public abstract class NewTextualShipComponent : NewShipComponent {
             status = string.Join("\n", newStatus.ToArray());
         }
         statusText.text = status;
+    }
+
+    protected override float SetProcessingDelay() {
+        if (stateChanged) {
+            return DEFAULT_DELAY;
+        }
+        return 0f;
     }
 
     override public void SetGlow(bool isGlowing) {
