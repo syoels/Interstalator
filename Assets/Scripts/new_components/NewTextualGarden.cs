1ab46394dd680bf63b4b366064a3b0c7128c3eee
Nadav Sheffer <shefferu@gmail.com>
2017-06-24 02:29:36 +0300

    Added textual garden script

---
+++ Assets/Scripts/new_components/NewTextualGarden.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Interstalator {
+public class NewTextualGarden : NewTextualShipComponent {
+    protected override ElementTypes[][] DefineInputs() {
+        return new ElementTypes[][] { new ElementTypes[] { ElementTypes.Water } };
+    }
+
+    protected override NewShipComponentOutput[] InnerProcess() {
+        if (inputs[0].amount > 0) {
+            SetStatus("Being watered");
+        } else {
+            SetStatus("Not being watered");
+        }
+
+        return new NewShipComponentOutput[] {};
+    }
+}
+}
