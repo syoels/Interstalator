6206b26b7739d8051d11fc65c30f9e9891db202d
Nadav Sheffer <shefferu@gmail.com>
2017-06-24 02:55:48 +0300

    Copied more texutal components to the new design

---
+++ Assets/Scripts/new_components/NewTextualAirMachine.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Interstalator {
+public class NewTextualAirMachine : NewTextualShipComponent {
+    public float waterRequirement = 0.3f;
+    public float electricityRequirement = 1f;
+
+    protected override ElementTypes[][] DefineInputs() {
+        return new ElementTypes[][]
+        {
+            new ElementTypes[] { ElementTypes.Water, ElementTypes.Poison },
+            new ElementTypes[] { ElementTypes.Electricity }
+        };
+    }
+
+    protected override NewShipComponentOutput[] InnerProcess() {
+        bool notEnoughWater = inputs[0].amount < waterRequirement;
+        bool notEnoughElectrcity = inputs[1].amount < electricityRequirement;
+
+        if (notEnoughWater || notEnoughElectrcity) {
+            GameManager.instance.shipStatus.SetProblem(
+                ShipSystem.Air,
+                "No air!"
+            );
+            if (notEnoughElectrcity) {
+                SetStatus("Not enough electricity");
+            } else {
+                SetStatus("Not enough water");
+            }
+        } else if (inputs[0].type == ElementTypes.Poison) {
+            GameManager.instance.shipStatus.SetProblem(
+                ShipSystem.Air,
+                "Posioned!"
+            );
+            SetStatus("Generating poisoned air");
+        } else {
+            GameManager.instance.shipStatus.SetOk(
+                ShipSystem.Air
+            );
+            SetStatus("Generating air");
+        }
+
+        // Not outputting anything
+        return new NewShipComponentOutput[] {};
+    }
+}
+}
---
+++ Assets/Scripts/new_components/NewTextualGenericSwitch.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Interstalator {
+public class NewTextualGenericSwitch : NewTextualShipComponent {
+    [SerializeField] private ElementTypes[] switchElements;
+    [SerializeField] private float[] distribution;
+
+    protected override ElementTypes[][] DefineInputs() {
+        return new ElementTypes[][] { switchElements };
+    }
+
+    private void InitDistribution(){
+        if (distribution.Length != children.Length) {
+            distribution = new float[children.Length];
+            float division = 1f / children.Length;
+            for (int i = 0; i < children.Length; i++) {
+                distribution[i] = division;
+            }
+        }
+        ApplyDistribution(distribution);
+    }
+
+    public void ApplyDistribution(float[] newDistribution) {
+        Debug.Assert(newDistribution.Length == children.Length);
+        distribution = newDistribution;
+        // Used to avoid referenceing null object at the start of the game
+        if (GameManager.instance != null) {
+            GameManager.instance.Flow();
+        }
+    }
+
+
+    protected override NewShipComponentOutput[] InnerProcess() {
+        ElementTypes passType = inputs[0].type;
+        float amount = inputs[0].amount;
+
+        for (int i = 0; i < outputs.Length; i++) {
+            NewShipComponentOutput output = outputs[i];
+            output.Set(passType, distribution[i] * inputs[0].amount);
+        }
+
+        string textualDistribution = "";
+        foreach (float percentage in distribution) {
+            textualDistribution += (int)(percentage * 100) + "% - ";
+        }
+        SetStatus("Distributing " + amount + " " + passType + " by " +
+            // Removing the extra ' - ' from the distribution text
+            textualDistribution.Substring(0, textualDistribution.Length - 3));
+        return outputs;
+    }
+}
+}
