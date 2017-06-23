939bd58bd352ceebdcd5809e9e1df2314459b393
Nadav Sheffer <shefferu@gmail.com>
2017-06-24 01:11:36 +0300

    Finished the basic code for the main ShipComponent's new design

--- Assets/Scripts/new_components/NewShipComponent.cs
+++ Assets/Scripts/new_components/NewShipComponent.cs
@@ -3,14 +3,52 @@ using System.Collections;
 
 namespace Interstalator {
 public abstract class NewShipComponent : MonoBehaviour {
-
     [SerializeField]private NewShipComponent[] children;
     private NewShipComponentInput[] inputs;
     private NewShipComponentOutput[] outputs;
+    private bool _isProcessing;
+    public bool isProcessing {
+        get {
+            foreach (NewShipComponent child in children) {
+                if (child.isProcessing) {
+                    return true;
+                }
+            }
+            return _isProcessing;
+        }
+    }
 
+    void Awake() {
+        // Define inputs and outputs
+        outputs = new NewShipComponentOutput[children.Length]();
+        for (int i = 0; i < children.Length; i++) {
+            NewShipComponent child = children[i];
+            outputs[i] = new NewShipComponentOutput(child);
+        }
 
+        ElementTypes[][] possibleInputTypes = DefineInputs();
+        inputs = new NewShipComponentInput[possibleInputTypes.Length]();
+        for (int i = 0; i < possibleInputTypes.Length; i++) {
+            inputs[i] = new NewShipComponentInput(possibleInputTypes[i]);
+        }
+    }
+
+    // Used by ship component to build the initial inputs list
+    virtual protected ElementTypes[][] DefineInputs() {
+        return new ElementTypes[0][];
+    }
+
+    // Mark inputs as not received to allow running the next flow
+    public void ResetInputs() {
+        foreach (NewShipComponentInput input in inputs) {
+            input.received = false;
+        }
+    }
+
+    // Insert new inputs to the component
     public int UpdateInput(ElementTypes type, float amount, int index=null) {
         if (index != null && !inputs[index].received) {
+            // TODO: Maybe check here if inputs have changed
             inputs[index].type = type;
             inputs[index].amount = amount;
         } else {
@@ -29,6 +67,38 @@ public abstract class NewShipComponent : MonoBehaviour {
             gameObject.name  + " does not have available input for given type: " + type
         );
         return null;
+    }
+
+    public int GetAwaitingInputs() {
+        int waitingInputs = 0;
+        foreach (NewShipComponentInput input in inputs) {
+            if (!input.received) {
+                waitingInputs++;
+            }
+        }
+        return waitingInputs;
+    }
+
+    // Main function that holds the logic for each component
+    protected abstract NewShipComponentOutput[] InnerProcess();
+
+    public IEnumerator Process () {
+        Debug.Assert(GetAwaitingInputs() == 0);
+        yield return new WaitUntil(!isProcessing);
+        _isProcessing = true;
+
+        NewShipComponentOutput[] outputs = InnerProcess();
+        // TODO: Add delay here
+
+        foreach (NewShipComponentOutput output in outputs) {
+            output.Send();
+            NewShipComponent child = output.component;
+            if (child.GetAwaitingInputs() == 0) {
+                StartCoroutine(child.Process());
+            }
+        }
+        _isProcessing = false;
+
     }
 }
 }
--- Assets/Scripts/new_components/NewShipComponentOutput.cs
+++ Assets/Scripts/new_components/NewShipComponentOutput.cs
@@ -1,26 +1,28 @@
-﻿
+﻿using UnityEngine;
+
 namespace Interstalator {
 public class NewShipComponentOutput {
-    private NewShipComponent component;
+    private NewShipComponent _component;
+    public NewShipComponent component { get { return _component; } }
     private int inputIndex;
     private ElementTypes type;
     private float amount;
 
-    public NewShipComponentOutput(ShipComponent comp, ElementTypes type, float amount, int index=null) {
-        this.component = comp; 
-        this.type = type; 
-        this.amount = amount;
-        this.inputIndex = index;
+    public NewShipComponentOutput(ShipComponent comp) {
+        this._component = comp; 
     }
 
-    public void Send(ElementTypes type, float amount) {
+    public void Set(ElementTypes type, float amount) {
         this.type = type;
         this.amount = amount;
+    }
 
+    public void Send() {
+        Debug.Assert(type != null && amount != null, "Output parameters not defined", this);
         if (this.inputIndex != null) {
-            component.UpdateInput(this.type, this.amount, inputIndex);
+            _component.UpdateInput(this.type, this.amount, inputIndex);
         } else {
-            inputIndex = component.UpdateInput(this.type, this.amount);
+            inputIndex = _component.UpdateInput(this.type, this.amount);
         }
     }
 }
