b91d5dcf405943c29dafbefee51169b8253123b1
syoels <syoels@gmail.com>
2017-06-25 02:55:38 +0300

    window working :) with aliens and all

--- Assets/Scripts/symptoms/SpaceshipWindow.cs
+++ Assets/Scripts/symptoms/SpaceshipWindow.cs
@@ -10,7 +10,7 @@ public class SpaceshipWindow : MonoBehaviour {
     [SerializeField] int aliens = 0; 
     [SerializeField] int maxAliens = 4; 
     public GameObject alienPrefab; 
-    private List<GameObject> aliensList; 
+    [SerializeField] List<GameObject> aliensList; 
     private GameObject aliensParent; 
 
     // Animation related
@@ -19,6 +19,10 @@ public class SpaceshipWindow : MonoBehaviour {
     private int shipDriveParamInt; 
     private int shipStopParamInt; 
     private int alienLeaveParamInt; 
+    private float windowSize; 
+    private float aliensXDistance;
+    private float alienSpawnInterval = 4f;
+    Vector3 firstAlienPosition; 
 
     // Speed
     private float maxSpeed = 1f; 
@@ -37,44 +41,66 @@ public class SpaceshipWindow : MonoBehaviour {
             // Ship stop
             if (prevSpeed > speedThreshold && speed <= speedThreshold) {
                 anim.SetTrigger(shipStopParamInt);
+                for (int i = 1; i <= maxAliens; i++) {
+                    Invoke("AddAlien", i * alienSpawnInterval);
+                }
             } 
             // Ship Accelerate
             else if (prevSpeed <= speedThreshold && speed > speedThreshold){
                 anim.SetTrigger(shipDriveParamInt);
+                CancelInvoke();
+                RemoveAliens();
             }
         }
     }
 
 
+    /// <summary>
+    /// Initialize all relavant variables: 
+    /// Animator, animator ints, aliens parent object etc. 
+    /// </summary>
     void Awake(){
         anim = GetComponent<Animator>();
         speedParamInt = Animator.StringToHash("speed");
+        aliensList = new List<GameObject>();
         alienLeaveParamInt = Animator.StringToHash("leave");
         shipDriveParamInt = Animator.StringToHash("drive");
         shipStopParamInt = Animator.StringToHash("stop");
         aliensParent = transform.Find("Graphics").gameObject;
+        windowSize = GetComponentInChildren<SpriteRenderer>().bounds.size.x;
+        aliensXDistance = windowSize / maxAliens - 0.75f; // -0.75 just  in case 
+        firstAlienPosition = transform.position - new Vector3(5f, 1f, 0f);
     }
 
+
+    /// <summary>
+    /// Adds an "Alien" child to "Graphics". 
+    /// Alien is a prefab with it's own animator. 
+    /// </summary>
     private void AddAlien(){
-        if (aliens >= maxAliens) {
+        if (aliens >= maxAliens || speed > speedThreshold) {
             return; 
         }
-        var clone = Instantiate(alienPrefab, new Vector3(0f, 0f, 0f), Quaternion.identity);
+        GameObject clone = Instantiate(alienPrefab, firstAlienPosition + new Vector3(aliens * aliensXDistance, 0f, 0f), Quaternion.identity) as GameObject;
         clone.transform.parent = aliensParent.transform;
         aliensList.Add(clone);
         aliens++; 
     }
 
-    private void RemoveAlien(){
-        if (aliens <= 0) {
+
+    /// <summary>
+    /// removes all aliens from the window
+    /// </summary>
+    private void RemoveAliens(){
+        if (aliens <= 0 || aliensList.Count == 0) {
             return;
         }
-        GameObject removeAlien = aliensList[0];
-        Animator alienAnimator = removeAlien.transform.GetComponent<Animator>();
-        alienAnimator.SetTrigger(alienLeaveParamInt);
-        aliensList.RemoveAt(0);
-        //Destroy(removeAlien); //TODO: replace with object pool? or maybe fixed number of aliens disabled & re-enabled ? 
-        aliens--;
+        foreach (GameObject alienObj in aliensList) {
+            Animator alienAnimator = alienObj.transform.GetComponent<Animator>();
+            alienAnimator.SetTrigger(alienLeaveParamInt);
+            aliens--;
+        }
+        aliensList.Clear();
     }
 
 
