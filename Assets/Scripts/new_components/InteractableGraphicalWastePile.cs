bc3ac6b46093ac85e1c223d8247377bf3ed62060
Nadav Sheffer <shefferu@gmail.com>
2017-06-27 20:22:24 +0300

    Added waste pile prefab and basic interactability - needs to add item handeling

---
+++ Assets/Scripts/new_components/InteractableGraphicalWastePile.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Interstalator {
+[RequireComponent(typeof (NewGraphicalWastePile))]
+public class InteractableGraphicalWastePile : Interactable {
+
+    private NewGraphicalWastePile wastePile {
+        get { return (NewGraphicalWastePile)relComponent; }
+    }
+
+    public override bool IsInteractable() {
+        return wastePile.pileSize > 0;
+    }
+
+    public override void Interact() {
+        wastePile.pileSize--;
+    }
+
+    public override string GetInteractionText() {
+        return "Pick nuclear waste from pile";
+    }
+}
+}
