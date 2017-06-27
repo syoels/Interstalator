5a4f80d0a6ec7e8a3213bdcc705a9a4d7f247c94
syoels <syoels@gmail.com>
2017-06-27 18:05:31 +0300

    prepare ground for garden... (component with animator params but no animator states yet)

---
+++ Assets/Scripts/new_components/NewGraphicalGarden.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+namespace Interstalator {
+public class NewGraphicalGarden : NewGraphicalShipComponent {
+   
+    private int elementParamId;
+    private int amountParamId;
+
+    protected override ElementTypes[][] DefineInputs() {
+        return new ElementTypes[1][]
+        {
+            new ElementTypes[] {ElementTypes.Water, ElementTypes.Poison}
+        };
+    }
+
+    protected override void SetAnimationParameterIds() {
+        base.SetAnimationParameterIds();
+        elementParamId = Animator.StringToHash("Element");
+        amountParamId = Animator.StringToHash("Amount");
+    }
+
+    protected override NewShipComponentOutput[] InnerProcess() {
+
+        float amount = inputs[0].amount;
+        ElementTypes type = inputs[0].type;
+        animator.SetInteger(elementParamId, animatorElements[type]);
+        animator.SetFloat(amountParamId, amount);
+
+        return outputs;
+    }
+
+}
+}
+
