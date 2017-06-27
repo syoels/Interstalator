201f09145fe6250f4d2d6b59257c8c70aabe1d0c
Nadav Sheffer <shefferu@gmail.com>
2017-06-27 22:11:36 +0300

    Added graphical item and added a parent "Item" class

---
+++ Assets/Scripts/new_components/GraphicalItem.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Interstalator {
+public class GraphicalItem : Item {
+    private Transform glowObj;
+
+    void Awake() {
+        glowObj = transform.Find("Glow");
+    }
+
+    override public void SetGlow(bool isGlowing) {
+        if (glowObj != null) {
+            glowObj.gameObject.SetActive(isGlowing);
+        }
+    }
+}
+}
---
+++ Assets/Scripts/new_components/Item.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Interstalator {
+public enum ItemType {
+    // Names have underscore to easily print them in human readable format
+    None, Water_Hoze, Tape, Nuclear_Waste
+}
+
+public abstract class Item : MonoBehaviour, Interactable {
+    public ItemType type;
+
+    public override string ToString() {
+        return type.ToString().Replace("_", " ");
+    }
+
+    public bool IsInteractable() {
+        return GameManager.instance.itemManager.heldItemType == ItemType.None;
+    }
+
+    public void Interact() {
+        GameManager.instance.itemManager.heldItem = this;
+    }
+
+    public string GetInteractionText() {
+        return "Pick up " + this;
+    }
+
+    // Graphical items should override this
+    virtual public void SetGlow(bool isGlowing) {
+        return;
+    }
+}
+}
