5dd5eabb1aaac599573174d23c729e60e8e0d31b
Nadav Sheffer <shefferu@gmail.com>
2017-06-27 22:28:04 +0300

    Added graphical item manager and added parent item manager script

---
+++ Assets/Scripts/managers/GraphicalItemManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Interstalator {
+public class GraphicalItemManager : ItemManager {
+
+    override public Item heldItem {
+        get {
+            return _heldItem;
+        }
+        set {
+            _heldItem = value;
+            if (_heldItem == null) {
+                SetItemText("None");
+            } else {
+                SetItemText(_heldItem.ToString());
+                _heldItem.transform.SetParent(GameManager.instance.player.transform);
+//                _heldItem.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    override public void DropItem() {
+        // TODO: Write graphical logic
+        // Can't drop an item if not holding anything
+        Debug.Assert(heldItem != null);
+//        TextualItem item = heldItem;
+//        heldItem = null;
+//        item.gameObject.SetActive(true);
+        return;
+    }
+}
+}
---
+++ Assets/Scripts/managers/TextualItemManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace Interstalator {
+/// <summary>
+/// Manages item holding, dropping and displaying. Only relevant for textual game since
+/// player script should be in charge of these.
+/// </summary>
+public class TextualItemManager : ItemManager {
+    private GameObject dropButton;
+
+    /// <summary>
+    /// Gets or sets the held item. If None is received instead of a TextualItem -
+    /// sets the current item to non.
+    /// </summary>
+    /// <value>The held item.</value>
+    override public Item heldItem {
+        get {
+            return _heldItem;
+        }
+        set {
+            _heldItem = value;
+            if (_heldItem == null) {
+                SetItemText("None");
+                dropButton.SetActive(false);
+            } else {
+                SetItemText(_heldItem.ToString());
+                _heldItem.gameObject.SetActive(false);
+                dropButton.SetActive(true);
+                GameManager.instance.interactionDisplay.Set("None");
+            }
+        }
+    }
+
+    new void Awake() {
+        base.Awake();
+        dropButton = itemPanel.transform.Find("Drop").gameObject;
+        // Could be set via editor but this makes sure the button works
+        Button dropButtonScript = dropButton.GetComponent<Button>();
+        dropButtonScript.onClick.AddListener(DropItem);
+    }
+
+    /// <summary>
+    /// Drops the currently held item - should not be activated when no items are held
+    /// </summary>
+    override public void DropItem() {
+        // Can't drop an item if not holding anything
+        Debug.Assert(heldItem != null);
+        Item item = heldItem;
+        heldItem = null;
+        item.gameObject.SetActive(true);
+        return;
+    }
+}
+}
