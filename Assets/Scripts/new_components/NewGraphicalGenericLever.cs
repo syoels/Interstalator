f36381171cd9dd43680305597ea98bff8e0fd7d2
syoels <syoels@gmail.com>
2017-06-27 20:09:34 +0300

    Generic Lever, Water Lever prefab, fixed some aniamtion issues, updated graphical level

---
+++ Assets/Scripts/new_components/NewGraphicalGenericLever.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+namespace Interstalator {
+public class NewGraphicalGenericLever : NewGraphicalShipComponent, Toggleable {
+
+    public ElementTypes[] possibleTypes;
+    private int CurrInputParamId;
+    [SerializeField] private int currInput = 0; 
+
+
+    protected override ElementTypes[][] DefineInputs() {
+        return new ElementTypes[2][] { possibleTypes, possibleTypes };
+    }
+
+    protected override void SetAnimationParameterIds() {
+        base.SetAnimationParameterIds();
+        CurrInputParamId = Animator.StringToHash("Input");
+    }
+
+    protected override NewShipComponentOutput[] InnerProcess() {
+
+        float amount = inputs[currInput].amount;
+        ElementTypes type = inputs[currInput].type;
+        return DistributeAmongChildren(type, amount);
+    }
+
+    public void Toggle() {
+        currInput = (currInput + 1) % inputs.Length;
+        animator.SetInteger(CurrInputParamId, currInput);
+        GameManager.instance.Flow();
+    }
+}
+}
