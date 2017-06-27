b471c16cafeaf198fb1727f812725da2c22b44df
syoels <syoels@gmail.com>
2017-06-27 20:30:15 +0300

    started writing graphical waste disposal machine

---
+++ Assets/Scripts/new_components/NewGraphicalWasteDisposalMachine.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+
+namespace Interstalator {
+public class NewGraphicalWasteDisposalMachine : NewGraphicalShipComponent {
+
+    [SerializeField] private bool open;
+    private int inParamId; 
+
+
+    protected override ElementTypes[][] DefineInputs() {
+        return new ElementTypes[1][]
+        {
+            new ElementTypes[] {ElementTypes.WastePerSecond}
+        };
+    }
+
+    protected override void SetAnimationParameterIds() {
+        base.SetAnimationParameterIds();
+        inParamId = Animator.StringToHash("In");
+    }
+
+    protected override NewShipComponentOutput[] InnerProcess() {
+        return outputs;
+    }
+
+
+    public void Open(){
+        open = true; 
+    }
+
+    public void Close(){
+        open = false; 
+    }
+
+    public void onWasteReachOpen(){
+        animator.SetBool(inParamId, open);
+    }
+
+   
+
+
+}
+}
+
